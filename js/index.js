41f8765d0f3564e4c203ac9051fb6dac5ec1f83d
muhammad iqbal fauzi <iqbal.fauzi9331@gmail.com>
2019-10-03 15:27:30 +0700

    arbitrary size

--- js/index.js
+++ js/index.js
@@ -1,4 +1,66 @@
 // JavaScript Document
+const reset = () => {
+    $("#game li").text("+");
+    $("#game li").removeClass("disable");
+    $("#game li").removeClass("o");
+    $("#game li").removeClass("x");
+    $("#game li").removeClass("btn-primary");
+    $("#game li").removeClass("btn-info");
+    count = 0;
+};
+
+const isOWon = () =>
+    ($("#one").hasClass("o") &&
+        $("#two").hasClass("o") &&
+        $("#three").hasClass("o")) ||
+    ($("#four").hasClass("o") &&
+        $("#five").hasClass("o") &&
+        $("#six").hasClass("o")) ||
+    ($("#seven").hasClass("o") &&
+        $("#eight").hasClass("o") &&
+        $("#nine").hasClass("o")) ||
+    ($("#one").hasClass("o") &&
+        $("#four").hasClass("o") &&
+        $("#seven").hasClass("o")) ||
+    ($("#two").hasClass("o") &&
+        $("#five").hasClass("o") &&
+        $("#eight").hasClass("o")) ||
+    ($("#three").hasClass("o") &&
+        $("#six").hasClass("o") &&
+        $("#nine").hasClass("o")) ||
+    ($("#one").hasClass("o") &&
+        $("#five").hasClass("o") &&
+        $("#nine").hasClass("o")) ||
+    ($("#three").hasClass("o") &&
+        $("#five").hasClass("o") &&
+        $("#seven").hasClass("o"));
+
+const isXWon = () =>
+    ($("#one").hasClass("x") &&
+        $("#two").hasClass("x") &&
+        $("#three").hasClass("x")) ||
+    ($("#four").hasClass("x") &&
+        $("#five").hasClass("x") &&
+        $("#six").hasClass("x")) ||
+    ($("#seven").hasClass("x") &&
+        $("#eight").hasClass("x") &&
+        $("#nine").hasClass("x")) ||
+    ($("#one").hasClass("x") &&
+        $("#four").hasClass("x") &&
+        $("#seven").hasClass("x")) ||
+    ($("#two").hasClass("x") &&
+        $("#five").hasClass("x") &&
+        $("#eight").hasClass("x")) ||
+    ($("#three").hasClass("x") &&
+        $("#six").hasClass("x") &&
+        $("#nine").hasClass("x")) ||
+    ($("#one").hasClass("x") &&
+        $("#five").hasClass("x") &&
+        $("#nine").hasClass("x")) ||
+    ($("#three").hasClass("x") &&
+        $("#five").hasClass("x") &&
+        $("#seven").hasClass("x"));
+
 $(document).ready(function() {
     var x = "x";
     var o = "o";
@@ -7,85 +69,23 @@ $(document).ready(function() {
     var x_win = 0;
     var size = 3;
 
-    const reset = () => {
-        $("#game li").text("+");
-        $("#game li").removeClass("disable");
-        $("#game li").removeClass("o");
-        $("#game li").removeClass("x");
-        $("#game li").removeClass("btn-primary");
-        $("#game li").removeClass("btn-info");
-        count = 0;
-    };
-
-    const isOWon = () =>
-        ($("#one").hasClass("o") &&
-            $("#two").hasClass("o") &&
-            $("#three").hasClass("o")) ||
-        ($("#four").hasClass("o") &&
-            $("#five").hasClass("o") &&
-            $("#six").hasClass("o")) ||
-        ($("#seven").hasClass("o") &&
-            $("#eight").hasClass("o") &&
-            $("#nine").hasClass("o")) ||
-        ($("#one").hasClass("o") &&
-            $("#four").hasClass("o") &&
-            $("#seven").hasClass("o")) ||
-        ($("#two").hasClass("o") &&
-            $("#five").hasClass("o") &&
-            $("#eight").hasClass("o")) ||
-        ($("#three").hasClass("o") &&
-            $("#six").hasClass("o") &&
-            $("#nine").hasClass("o")) ||
-        ($("#one").hasClass("o") &&
-            $("#five").hasClass("o") &&
-            $("#nine").hasClass("o")) ||
-        ($("#three").hasClass("o") &&
-            $("#five").hasClass("o") &&
-            $("#seven").hasClass("o"));
-
-    const isXWon = () =>
-        ($("#one").hasClass("x") &&
-            $("#two").hasClass("x") &&
-            $("#three").hasClass("x")) ||
-        ($("#four").hasClass("x") &&
-            $("#five").hasClass("x") &&
-            $("#six").hasClass("x")) ||
-        ($("#seven").hasClass("x") &&
-            $("#eight").hasClass("x") &&
-            $("#nine").hasClass("x")) ||
-        ($("#one").hasClass("x") &&
-            $("#four").hasClass("x") &&
-            $("#seven").hasClass("x")) ||
-        ($("#two").hasClass("x") &&
-            $("#five").hasClass("x") &&
-            $("#eight").hasClass("x")) ||
-        ($("#three").hasClass("x") &&
-            $("#six").hasClass("x") &&
-            $("#nine").hasClass("x")) ||
-        ($("#one").hasClass("x") &&
-            $("#five").hasClass("x") &&
-            $("#nine").hasClass("x")) ||
-        ($("#three").hasClass("x") &&
-            $("#five").hasClass("x") &&
-            $("#seven").hasClass("x"));
-
-    $("#board-size-input").on("change", function() {});
-    $("#game li").click(function() {
+    click = thisObj => {
         if (isOWon()) {
             alert("O has won the game. Start a new game");
-            reset()
+            reset();
         } else if (isXWon()) {
             alert("X wins has won the game. Start a new game");
-            reset()
-        } else if (count == 9) {
+            reset();
+        } else if (count == size * size) {
             alert("Its a tie. It will restart.");
             reset();
         } else if ($(this).hasClass("disable")) {
             alert("Already selected");
         } else if (count % 2 == 0) {
+            console.log("a");
             count++;
-            $(this).text(o);
-            $(this).addClass("disable o btn-primary");
+            thisObj.text(o);
+            thisObj.addClass("disable o btn-primary");
             if (isOWon()) {
                 alert("O wins");
                 count = 0;
@@ -94,8 +94,8 @@ $(document).ready(function() {
             }
         } else {
             count++;
-            $(this).text(x);
-            $(this).addClass("disable x btn-info");
+            thisObj.text(x);
+            thisObj.addClass("disable x btn-info");
             if (isXWon()) {
                 alert("X wins");
                 count = 0;
@@ -103,7 +103,28 @@ $(document).ready(function() {
                 $("#x_win").text(x_win);
             }
         }
+    };
+
+    $("#board-size-input").on("change", function(e) {
+        size = e.target.value;
+        reset();
+        $("#game").empty();
+        for (var i = 0; i < size * size; i++) {
+            $("#game").append(
+                $("<li>+</li>")
+                    .attr({ id: "" + (i + 1) })
+                    .addClass("btn span1")
+            );
+        }
+        $("#game li").click(function() {
+            click($(this));
+        });
     });
+
+    $("#game li").click(function() {
+        click($(this));
+    });
+
     $("#reset").click(function() {
         reset();
     });
